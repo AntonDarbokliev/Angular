62845367804befcdd5eb4b6fade822de6e97631b
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-01-19 19:09:17 +0200

    New components

--- my-app/src/app/movies/movies-main/movies.component.ts
+++ my-app/src/app/movies/movies-main/movies.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MoviesListComponent } from '../movies-list/movies-list.component';
+import { MoviesSearchComponent } from '../movies-search/movies-search.component';
+
 
 @Component({
   selector: 'app-movies',
   standalone: true,
-  imports: [],
+  imports: [MoviesListComponent, MoviesSearchComponent],
   templateUrl: './movies.component.html',
   styleUrl: './movies.component.scss'
 })
---
+++ my-app/src/app/movies/movies-search/movies-search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'movies-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './movies-search.component.html',
+  styleUrl: './movies-search.component.scss'
+})
+export class MoviesSearchComponent {
+
+}
